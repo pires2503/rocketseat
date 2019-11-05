5ec227ae813ed87b750a8a092ee14b05bcce1899
Maurício Pires Cardoso <mauricio@ema.net.br>
2019-11-05 15:26:41 -0300

    Buscando produtos da API

--- packages/reactjs-rocketshoes/src/pages/Cart/index.js
+++ packages/reactjs-rocketshoes/src/pages/Cart/index.js
@@ -14,35 +14,41 @@ export default function Cart() {
       <ProductTable>
         <thead>
           <tr>
+            <th />
             <th>PRODUTO</th>
             <th>QTD</th>
             <th>SUBTOTAL</th>
+            <th />
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>
               <img src={Tenis} alt="Tenis" />
-              <td>
-                <strong>Tenis muito massa</strong>
-                <span>R$129,90</span>
-              </td>
             </td>
             <td>
-              <button type="button">
-                <MdRemoveCircleOutline size={20} color="#7159c1" />
-              </button>
-              <input type="number" readOnly value={2} />
-              <button type="button">
-                <MdAddCircleOutline size={20} color="#7159c1" />
-              </button>
+              <strong>Tenis muito massa</strong>
+              <span>R$129,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
             </td>
             <td>
               <strong>R$ 258,80</strong>
             </td>
-            <button type="button">
-              <MdDelete size={20} color="#7159c1" />
-            </button>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
           </tr>
         </tbody>
       </ProductTable>

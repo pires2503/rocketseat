c1ef6d10d89164e070adad5c963429dfcbdd035f
Maurício Pires Cardoso <mauricio@ema.net.br>
2019-11-06 15:34:52 -0300

    Verificar estoque

--- packages/reactjs-rocketshoes/src/store/modules/cart/sagas.js
+++ packages/reactjs-rocketshoes/src/store/modules/cart/sagas.js
@@ -9,9 +9,20 @@ function* addToCart({ id }) {
     state.cart.find(p => p.id === id)
   );
 
+  // Verifica o estoque do produto
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO: Não há produto em estoque!');
+    return;
+  }
+
   // Se existir alterar apenas a quantidade
   if (productExists) {
-    const amount = productExists.amount + 1;
+    // const amount = productExists.amount + 1;
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);

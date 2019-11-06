19cb0e91fd3bfdedf79832afd6c97b631d8be92d
Maurício Pires Cardoso <mauricio@ema.net.br>
2019-11-06 14:57:48 -0300

    Configurando Redux Saga

--- packages/reactjs-rocketshoes/src/store/index.js
+++ packages/reactjs-rocketshoes/src/store/index.js
@@ -1,10 +1,21 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import combineReducers from './modules/combineReducer';
+import combineSaga from './modules/combineSaga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const enhancer =
-  process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+  process.env.NODE_ENV === 'development'
+    ? compose(
+        console.tron.createEnhancer(),
+        applyMiddleware(sagaMiddleware)
+      )
+    : applyMiddleware(sagaMiddleware);
 
 const store = createStore(combineReducers, enhancer);
+
+sagaMiddleware.run(combineSaga);
 
 export default store;
